25c4543795e9ef4abfb2a01b4088228d40b015e7
Cleveland Shaw <shawshaw76@gmail.com>
2024-06-28 19:14:48 +0100

    Removed unwanted code

--- app/src/main/java/com/pelagohealth/codingchallenge/presentation/PelagoApp.kt
+++ app/src/main/java/com/pelagohealth/codingchallenge/presentation/PelagoApp.kt
@@ -4,9 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PelagoApp : Application() /*{
-	override fun onCreate() {
-		super.onCreate()
-*//*		initKoin()*//*
-	}
-}*/
+class PelagoApp : Application()
